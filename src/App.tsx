8226b1d90a813c824dc3caca139817a6fc41f476
Vignesh Ravimurugan <vigneshwar.ravimurugan@gmail.com>
2025-10-16 12:10:20 -0500

    UI changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import { useState } from 'react'
 import './App.css'
 import { ControlPanel } from './ControlPanel'
 import { CursorPanel } from './CursorPanel'
+import { PictureManager } from './PictureManager'
 import { defaultRoom, generateRandomEmoji } from './utils'
 
 function App() {
   const [username, setUsername] = useState<string>(generateRandomEmoji())
   const [currentRoomId, setCurrentRoomId] = useState<string>(defaultRoom.id)
   
+  
   return (
     <>
       <div className='cursor-panel'>
         <div className='info-panel'>
           <span>
+            <PictureManager roomId={currentRoomId} />
             Move cursor around to broadcast cursor position to others in the room.
             <br />
             Built with <a href="https://docs.amplify.aws/gen2">AWS Amplify Gen 2</a>.
